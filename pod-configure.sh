5b17e18f279fc9811908a591c01b888c7497e9f9
Dmytrii Puzyr <slymahel@gmail.com>
2026-03-21 11:39:47 +0000

    Add GH CLI setup to pod-configure

--- pod-configure.sh
+++ pod-configure.sh
@@ -1,9 +1,13 @@
 # Install deps
 curl -LsSf https://astral.sh/uv/install.sh | sh
 apt-get update
-apt-get install unzip
+apt-get install -y unzip gh
 
 # Setup repos
 git clone https://github.com/yugisu/SatDiFuser.git && cd SatDiFuser && git checkout research && cd ..
 
 git clone https://github.com/yugisu/diffusion-vpr.git && cd diffusion-vpr && uv sync
+
+# Auth
+# gh auth login --web
+# gh auth setup-git
